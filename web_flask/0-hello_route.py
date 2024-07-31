e49c292c408e3c77e9d40d8a316f74199428984b
Omarfathi22 <ommarf525.com>
2024-07-31 21:00:38 +0300

    error

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-""" Simple Flask web application that displays 'Hello HBNB!' """
+"""
+Flask web application script that listens on 0.0.0.0:5000 and
+responds with 'Hello HBNB!' at the root route.
+"""
 
 from flask import Flask
-
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """ Returns a greeting message """
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
 
