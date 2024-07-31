628e62149e4d0bb7ac0eeafb0a1868ed615b7558
Omarfathi22 <ommarf525.com>
2024-07-31 20:57:48 +0300

    0-task_fix

--- AirBnB_clone_v2/web_flask/0-hello_route.py
+++ AirBnB_clone_v2/web_flask/0-hello_route.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 """
-A simple Flask web application.
+Flask web application script that listens on 0.0.0.0:5000 and
+responds with 'Hello HBNB!' at the root route.
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
-def hello():
-    """
-    Displays "Hello HBNB!".
-    """
-    return "Hello HBNB!"
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+@app.route('/', strict_slashes=False)
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
