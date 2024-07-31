727adbe4b3ca2f69fce9a0fa332f965ae570504f
Omarfathi22 <ommarf525.com>
2024-07-31 21:08:12 +0300

    fix-2

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
 """
-Flask web application that starts a simple web server
+Flask web application script with multiple routes:
 """
 
 from flask import Flask
-
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+def index():
+    """returns Hello HBNB!"""
+    return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
-    return "HBNB"
+    """returns HBNB"""
+    return 'HBNB'
+
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    """Displays 'C ' followed by the value of the text variable
-    (replace underscore _ symbols with a space)"""
-    return "C {}".format(text.replace('_', ' '))
+def cisfun(text):
+    """display “C ” followed by the value of the text variable"""
+    return 'C ' + text.replace('_', ' ')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
 
